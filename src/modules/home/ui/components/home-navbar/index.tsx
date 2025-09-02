a0876c909c1b491b7a5557ba413c4dd57170004c
enzomendez315 <enzo.mendez315@gmail.com>
2025-09-01 21:40:23 -0600

    Add auth button to HomeNavbar.

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -1,6 +1,9 @@
-import { SidebarTrigger } from "@/components/ui/sidebar";
 import Link from "next/link";
 import Image from "next/image";
+
+import { AuthButton } from "@/modules/auth/ui/components/auth-button";
+import { SearchInput } from "./search-input";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export const HomeNavbar = () => {
   return (
@@ -10,9 +13,20 @@ export const HomeNavbar = () => {
         <div className="flex items-center flex-shrink-0">
           <SidebarTrigger />
           <Link href="/">
-            <Image src="/logo.svg" alt="Logo" width={32} height={32} />
-            <p>NewTube</p>
+            <div className="p-4 flex items-center gap-1">
+              <Image src="/logo.svg" alt="Logo" width={32} height={32} />
+              <p className="text-xl font-semibold tracking-tighter">NewTube</p>
+            </div>
           </Link>
+        </div>
+
+        {/* Search Bar */}
+        <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
+          <SearchInput />
+        </div>
+
+        <div className="flex-shrink-0 items-center flex gap-4">
+          <AuthButton />
         </div>
       </div>
     </nav>
