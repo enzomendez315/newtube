4c061549a949cfe7e73d280ea2bfbdde89c96483
enzomendez315 <enzo.mendez315@gmail.com>
2025-09-06 23:50:42 -0600

    Add UI sidebar.

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,15 +2,11 @@ import { HomeLayout } from "@/modules/home/ui/layouts/home-layout";
 import { ReactNode } from "react";
 
 interface LayoutProps {
-    children: ReactNode;
-};
+  children: ReactNode;
+}
 
 const Layout = ({ children }: LayoutProps) => {
-    return (
-        <HomeLayout>
-            {children}
-        </HomeLayout>
-    );
+  return <HomeLayout>{children}</HomeLayout>;
 };
 
 export default Layout;
